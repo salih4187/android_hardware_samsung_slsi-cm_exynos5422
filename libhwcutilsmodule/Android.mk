439320ade531f00d0ac52d2c2651d578da9e6b46
Salih Çağlar <111198755+salih4187@users.noreply.github.com>
2023-10-12 16:47:59 +0300

    Update Android.mk

--- libhwcutilsmodule/Android.mk
+++ libhwcutilsmodule/Android.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2008 The Android Open Source Project
+# Copyright (C) 2015 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,36 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ifeq ($(filter-out exynos5,$(TARGET_BOARD_PLATFORM)),)
-
-LOCAL_PATH:= $(call my-dir)
-include $(CLEAR_VARS)
-
-LOCAL_SHARED_LIBRARIES := liblog libutils libcutils libexynosutils libexynosv4l2 libsync libhwcutils libexynosgscaler libmpp
-
-ifeq ($(BOARD_USES_VIRTUAL_DISPLAY), true)
-LOCAL_CFLAGS += -DUSES_VIRTUAL_DISPLAY
-endif
-
-LOCAL_C_INCLUDES := \
-	$(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include \
-	$(LOCAL_PATH)/../../exynos5/include \
-	$(TOP)/hardware/samsung_slsi/exynos/include \
-	$(TOP)/hardware/samsung_slsi/exynos/libexynosutils \
-	$(TOP)/hardware/samsung_slsi/$(TARGET_SOC)/include \
-	$(TOP)/hardware/samsung_slsi/$(TARGET_SOC)/libhwcmodule \
-	$(TOP)/hardware/samsung_slsi/exynos/libhwc \
-	$(TOP)/hardware/samsung_slsi/exynos/libhwcutils \
-	$(TOP)/hardware/samsung_slsi/exynos/libdisplay \
-	$(TOP)/hardware/samsung_slsi/exynos/libmpp
-
-LOCAL_ADDITIONAL_DEPENDENCIES += \
-	$(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
-
-LOCAL_SRC_FILES := \
-	ExynosMPPModule.cpp
-
-LOCAL_MODULE := libhwcutilsmodule
-include $(BUILD_SHARED_LIBRARY)
-
-endif
+LOCAL_SRC_FILES += \
+	./../../$(TARGET_SOC)/libhwcutilsmodule/ExynosMPPModule.cpp
